594efc89996f7c53c085dd78cf93ce4e09728b8d
Themiyama <87962141+Themiyama@users.noreply.github.com>
2022-06-01 12:56:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.owner = ['Themiya madushanka']
+global.premium = ['94781190844']
+global.ownernomer = '94781190844'
+global.ownername = 'Themiya madushanka'
+global.botname = 'Madushanka-MD'
+global.footer = '©Madushanka.'
+global.ig = 'https://github.com/vimuwa_sl_'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https://chat.whatsapp.com/KoI0cNHTuhoB8iCOxbZYlR'
+global.packname = 'Maduwa'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
